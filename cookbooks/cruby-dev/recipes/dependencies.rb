1093cebf1784a14c838a4e536aa714f15ccfa871
Daniel Ferreira <dferreira@datapipe.com>
2018-01-13 02:58:30 +0000

    Add credits.

--- cookbooks/cruby-dev/recipes/dependencies.rb
+++ cookbooks/cruby-dev/recipes/dependencies.rb
@@ -2,15 +2,20 @@
 # Cookbook Name:: cruby-dev
 # Recipe:: dependencies
 #
+# Install cruby development dependencies.
+#
 # Copyright 2018, Daniel Ferreira (danieldasilvaferreira@gmail.com)
 #
 # All rights reserved - Do Not Redistribute
+
+##
+# Credits: https://github.com/tonytonyjan/ruby-cookbook/blob/master/recipes/default.rb
 
 package_names = case node["platform"]
                 when "ubuntu", "debian"
                   execute "apt-get update" do
                     ignore_failure true
-                    not_if do ::File.exists?("/var/lib/apt/periodic/update-success-stamp") end
+                    not_if { ::File.exists?("/var/lib/apt/periodic/update-success-stamp") }
                   end
                   %w[autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev
                           zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev]
